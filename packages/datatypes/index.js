39bf5cec5ff61adb6c74067f4df48bc98e0292bd
Goldmund <matthewtzito@gmail.com>
2020-06-30 18:49:06 -0700

    more comments...

--- packages/datatypes/index.js
+++ packages/datatypes/index.js
@@ -6,7 +6,7 @@ const ObservableArray = require("./ObservableArray");
  *     properties (qua the Observable's execution context), and ubiquitous Vivisector-contingent properties (qua the macro execution context)
  */
 
-module.exports = (function (global) {
+module.exports = (function (global) {  // wrap in IIFE to align execution context in prospective 'non-Node' environments
     // mitigate need to use `new` keyword by returning a discrete function constructor 
     // to generate the object   
     var Observable = function(datatype, data, options) {
@@ -39,11 +39,11 @@ module.exports = (function (global) {
             if (options) {
                 
                 /* 
-                // ~destructure id and enforce unique filter~
-                //
-                // What's happening here is we are evaluating a ternary expression wrapped inside of an IIFE. 
-                // This IIFE accepts as input the `options` object and destructures the id inline. We are explicitly
-                // mapping the prop `uniqueIdentifier` and destructuring it away from the resolved IIFE.
+                    Destructure id and enforce unique filter
+                    
+                    What's happening here is we are evaluating a ternary expression wrapped inside of an IIFE. 
+                    This IIFE accepts as input the `options` object and destructures the id inline. We are explicitly
+                    mapping the prop `uniqueIdentifier` and destructuring it away from the resolved IIFE.
                 */
 
                 const { uniqueIdentifier } = (({ id }) => ({ uniqueIdentifier: observableKeys.includes(id.toString()) ? undefined : id, /* prop2, prop3... */ }))(options);
@@ -83,3 +83,16 @@ module.exports = (function (global) {
     global.Observable = global.Vx = Observable;
 
 }(global));
+/* 
+    Passing `global` in lieu of `this`
+
+    We pass the global object in lieu of `this` for a couple of very specific performance reasons:
+
+    1. JavaScript always performs scope 'lookups' from the current function's scope *upward* until it finds an identifier; 
+    if we pass `this` as `global` into an IIFE, the IIFE's execution is the only time our code will need to process a scope lookup beyond
+    the local scope for `this`. Subsequent references to `global` inside the IIFE will therefore never require a lookup beyond the local scope of the IIFE.
+
+    2. JS minifiers won't minify direct references to anything without the context of the IIFE; 
+    we may need this param as a point-of-reference.
+
+*/
