3502218c10e9a43a097101bc2f931eb2cb8cb5bd
goldmund <matthewtzito@gmail.com>
2021-10-26 11:28:27 -0700

    build: update ci/cd to use github workflows

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
 	coveragePathIgnorePatterns: ['src/utils/exceptions.ts'],
 	coverageThreshold: {
 		global: {
-			branches: 0,
-			functions: 0,
-			lines: 0,
-			statements: 0
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
 		}
 	},
 	errorOnDeprecated: true,
